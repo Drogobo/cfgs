3b4bb235bf4a66f9568ae9c800ed6fcae50d8938
Luke <Drogobo@noreply.codeberg.org>
2022-12-16 19:17:44 -0500

    install works on gentoo and apt now

--- install.sh
+++ install.sh
@@ -5,9 +5,13 @@
 COLOR='\033[0;32m'
 NOCOLOR='\033[0m'
 echo -e "${COLOR}Install software for my configs.${NOCOLOR}"
-sudo pacman -S --needed neovim rustup kitty plasma xorg neofetch flatpak firefox doas git base-devel python-pip luajit
+if [ -x "$(command -v apt-get)" ]; then sudo apt-get -y install doas flatpak neovim kitty xorg kde-standard git curl rust-all python neofetch fonts-hack
+elif [ -x "$(command -v pacman)" ]; then sudo pacman -S --needed neovim rustup kitty plasma-desktop xorg neofetch flatpak doas git base-devel python-pip luajit curl
+elif [ -x "$(command -v emerge)" ]; then sudo emerge -a app-editors/neovim dev-lang/rust sys-apps/flatpak x11-terms/kitty app-admin/doas dev-vcs/git app-misc/neofetch net-misc/curl dev-python/pip media-fonts/hack kde-plasma/plasma-desktop
+else echo "Cannot figure out how to insatll software on whatever this OS is.">&2; fi
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 pip3 install --user neovim
+if [ -x "$(command -v pacman)" ]; then
 echo -e "${COLOR}Installing paru-bin from the AUR.${NOCOLOR}"
 mkdir paru-bin
 git clone https://aur.archlinux.org/paru-bin.git
@@ -15,11 +19,16 @@ cd paru-bin
 makepkg -si
 cd ..
 rm -rf paru-bin/
+echo -e "${COLOR}Install more crap but from the AUR.${NOCOLOR}"
+paru -S --needed ttf-hack ttf-twemoji adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts ttf-ubraille qalculate-gtk
+else echo -e "${COLOR}Skipping AUR packages because you are not on arch.${NOCOLOR}">&2; fi
 echo -e "${COLOR}Copy configs to correct dirs.${NOCOLOR}"
 sudo cp -v -r paru.conf doas.conf pacman.conf /etc/
 cp -v -r .config/ .bashrc ~/
-echo -e "${COLOR}Install more crap but from the AUR.${NOCOLOR}"
-paru -S --needed ttf-hack ttf-twemoji adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts ttf-ubraille qalculate-gtk
 echo -e "${COLOR}Do a quick update to finalize.${NOCOLOR}"
-paru -Syu && flatpak update
+if [ -x "$(command -v apt)" ]; then doas apt update && doas apt upgrade
+elif [ -x "$(command -v pacman)" ]; then paru -Syu
+elif [ -x "$(command -v emerge)" ]; then doas emaint -a sync && doas emerge --ask --verbose --update --deep --changed-use @world
+else echo "Cannot figure out how to update on whatever this OS is.">&2; fi
+flatpak update
 echo -e "${COLOR}I also included a bonus wallpaper! It is in the same dir as this script. If you want to use it, it is called 'Woods.jpg'.${NOCOLOR}"
