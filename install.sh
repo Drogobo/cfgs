02ab54607236e8a871808275ecc79f349524c6f4
Luke <Drogobo@noreply.codeberg.org>
2023-05-16 08:46:53 -0400

    icons

--- install.sh
+++ install.sh
@@ -27,8 +27,8 @@ fi
 # SOFTWARE INSTALL
 if ! $copyonly; then
 	echo -e "${COLOR}Install software for my configs.${NOCOLOR}"
-	if [ -x "$(command -v pacman)" ]; then sudo pacman -S --needed neovim rustup kitty xorg playerctl pipewire neofetch flatpak doas git base-devel python-pip luajit curl zsh zsh-completions xcb-util-cursor arc-gtk-theme redshift xorg-xmodmap picom xorg-setxkbmap papirus-icon-theme xcursor-vanilla-dmz feh libxcb thunar p7zip flameshot ttf-hack && sudo chmod u+s "$(which fusermount)"
-	elif [ -x "$(command -v emerge)" ]; then sudo emerge -a app-editors/neovim dev-lang/rust-bin sys-apps/flatpak x11-terms/kitty app-admin/doas dev-vcs/git app-misc/neofetch net-misc/curl dev-python/pip media-fonts/hack sys-fs/fuse:0 sys-kernel/genkernel app-shells/zsh app-shells/zsh-completions app-shells/gentoo-zsh-completions x11-misc/xclip x11-libs/xcb-util-cursor x11-misc/redshift x11-misc/picom x11-apps/xmodmap x11-apps/setxkbmap media-gfx/feh x11-libs/libxcb media-gfx/flameshot xfce-base/thunar media-video/pipewire media-sound/playerctl x11-themes/vanilla-dmz-xcursors x11-themes/papirus-icon-theme x11-themes/arc-theme app-arch/p7zip
+	if [ -x "$(command -v pacman)" ]; then sudo pacman -S --needed neovim rustup kitty xorg playerctl pipewire neofetch flatpak doas git base-devel python-pip luajit curl zsh meson zsh-completions xcb-util-cursor arc-gtk-theme redshift xorg-xmodmap picom xorg-setxkbmap xcursor-vanilla-dmz feh libxcb thunar p7zip flameshot ttf-hack && sudo chmod u+s "$(which fusermount)"
+	elif [ -x "$(command -v emerge)" ]; then sudo emerge -a app-editors/neovim dev-lang/rust-bin sys-apps/flatpak x11-terms/kitty app-admin/doas dev-vcs/git app-misc/neofetch net-misc/curl dev-python/pip media-fonts/hack sys-fs/fuse:0 sys-kernel/genkernel app-shells/zsh app-shells/zsh-completions app-shells/gentoo-zsh-completions x11-misc/xclip x11-libs/xcb-util-cursor x11-misc/redshift x11-misc/picom x11-apps/xmodmap x11-apps/setxkbmap media-gfx/feh x11-libs/libxcb media-gfx/flameshot xfce-base/thunar media-video/pipewire media-sound/playerctl x11-themes/vanilla-dmz-xcursors x11-themes/arc-theme app-arch/p7zip dev-util/meson
 	else echo "${COLOR}Cannot figure out how to insatll software on whatever this OS is.${NOCOLOR}"; fi
 	curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 	pip3 install --user neovim
@@ -105,6 +105,29 @@ sudo cp -v stylish/cursor/index.html /usr/share/icons/default/index.theme
 cp -v -r .config/ .oh-my-zsh/ .zshrc ~/
 mkdir ~/Pictures
 cp -v Woods.jpg ~/Pictures/
+
+if ! $copyonly; then
+# PAPIRUS
+	git clone https://github.com/PapirusDevelopmentTeam/papirus-icon-theme.git
+	cd papirus-icon theme
+	cp ../papirus.diff ./papirus.diff
+	git apply papirus.diff
+	cd tools/
+	./build_color_folders.sh
+	cd ..
+	sudo make install
+	cd ..
+	rm -rf papirus-icon-theme/
+# GTK THEME
+	git clone https://codeberg.org/Drogobo/luke-theme
+	cd luke-theme
+	meson setup --prefix=/usr -Dthemes=gtk2,gtk3,gtk4 -Dvariants=dark build/
+	sudo meson install -C build/
+	cd ..
+	rm -rf luke-theme/
+else
+	echo -e "${COLOR}Skipping GTK and icon theme.${NOCOLOR}"
+fi
 
 if ! $copyonly; then
 # UPDATE SYSTEM
