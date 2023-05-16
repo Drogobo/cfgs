fd0675cd6a938add64a62a7923b2fcfac7453bd9
Luke <Drogobo@noreply.codeberg.org>
2023-05-16 14:44:21 -0400

    inkscape

--- install.sh
+++ install.sh
@@ -27,8 +27,8 @@ fi
 # SOFTWARE INSTALL
 if ! $copyonly; then
 	echo -e "${COLOR}Install software for my configs.${NOCOLOR}"
-	if [ -x "$(command -v pacman)" ]; then sudo pacman -S --needed neovim rustup kitty xorg playerctl pipewire neofetch flatpak doas git base-devel python-pip luajit curl zsh meson sassc zsh-completions xcb-util-cursor redshift xorg-xmodmap picom xorg-setxkbmap xcursor-vanilla-dmz feh libxcb thunar p7zip flameshot && sudo chmod u+s "$(which fusermount)"
-	elif [ -x "$(command -v emerge)" ]; then sudo emerge -a app-editors/neovim dev-lang/rust-bin sys-apps/flatpak x11-terms/kitty app-admin/doas dev-vcs/git app-misc/neofetch net-misc/curl dev-python/pip sys-fs/fuse:0 sys-kernel/genkernel app-shells/zsh app-shells/zsh-completions app-shells/gentoo-zsh-completions x11-misc/xclip x11-libs/xcb-util-cursor x11-misc/redshift x11-misc/picom x11-apps/xmodmap x11-apps/setxkbmap media-gfx/feh x11-libs/libxcb media-gfx/flameshot xfce-base/thunar media-video/pipewire media-sound/playerctl x11-themes/vanilla-dmz-xcursors app-arch/p7zip dev-util/meson dev-lang/sassc
+	if [ -x "$(command -v pacman)" ]; then sudo pacman -S --needed neovim rustup kitty xorg playerctl pipewire neofetch flatpak doas git base-devel python-pip luajit curl zsh meson sassc zsh-completions xcb-util-cursor redshift inkscape xorg-xmodmap picom xorg-setxkbmap xcursor-vanilla-dmz feh libxcb thunar p7zip flameshot && sudo chmod u+s "$(which fusermount)"
+	elif [ -x "$(command -v emerge)" ]; then sudo emerge -a app-editors/neovim dev-lang/rust-bin sys-apps/flatpak x11-terms/kitty app-admin/doas dev-vcs/git app-misc/neofetch net-misc/curl dev-python/pip sys-fs/fuse:0 sys-kernel/genkernel app-shells/zsh app-shells/zsh-completions app-shells/gentoo-zsh-completions x11-misc/xclip x11-libs/xcb-util-cursor x11-misc/redshift x11-misc/picom x11-apps/xmodmap x11-apps/setxkbmap media-gfx/feh x11-libs/libxcb media-gfx/flameshot xfce-base/thunar media-video/pipewire media-sound/playerctl x11-themes/vanilla-dmz-xcursors app-arch/p7zip dev-util/meson dev-lang/sassc media-gfx/inkscape
 	else echo "${COLOR}Cannot figure out how to insatll software on whatever this OS is.${NOCOLOR}"; fi
 	curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 	pip3 install --user neovim
@@ -118,8 +118,8 @@ if ! $copyonly; then
 	sudo make install
 	cd ..
 	rm -rf papirus-icon-theme/
-	wget -qO- https://git.io/papirus-folders-install | sh
-	papirus-folders -C luke --theme Papirus-Dark
+	chmod +x papirus-folders
+	./papirus-folders -C luke --theme Papirus-Dark
 # GTK THEME
 	git clone https://codeberg.org/Drogobo/luke-theme
 	cd luke-theme/
